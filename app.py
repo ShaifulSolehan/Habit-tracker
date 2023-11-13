deb9f41d717276ad33b828f67e3d819f144c3f72
ShaifulSolehan <shaifulsolehan@gmail.com>
2023-11-13 12:15:27 +0800

    First commit

---
+++ app.py
@@ -0,0 +1,29 @@
+import datetime
+from flask import Flask,render_template,request
+
+app = Flask(__name__,template_folder="templates")
+
+habits=["testing","testing 2"]
+
+@app.context_processor
+def add_cal_date_range():
+    def date_range(start: datetime.date):
+        dates = [start + datetime.timedelta(days=diff) for diff in range(-3,4)]
+        return dates
+    return {"date_range":date_range}
+
+@app.route("/")
+def home():
+    date_str = request.args.get("date")
+    if(date_str):
+        selected_date = datetime.date.fromisoformat(date_str)
+    else:
+        selected_date = datetime.date.today()
+
+    return render_template("home.html", habits=habits,title="Habit tracker - Home",selected_date=selected_date)
+
+@app.route("/add",methods=["GET","POST"])
+def add_habit():
+    if request.method == "POST":
+        habits.append(request.form.get("habit"))
+    return render_template("add_habit.html",title="Habit Tracker - Add Habit",selected_date=datetime.date.today())
