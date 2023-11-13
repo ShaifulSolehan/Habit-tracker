fc8b3edcb9d7c245c59fbddd44db21c6a4a8bd56
ShaifulSolehan <shaifulsolehan@gmail.com>
2023-11-13 16:21:31 +0800

    adding completion features and styling

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import datetime
-from flask import Flask,render_template,request
+from collections import defaultdict
+from flask import Flask,render_template,request,redirect,url_for
 
 app = Flask(__name__,template_folder="templates")
 
 habits=["testing","testing 2"]
+completions = defaultdict(list)
 
 @app.context_processor
 def add_cal_date_range():
@@ -20,10 +22,19 @@ def home():
     else:
         selected_date = datetime.date.today()
 
-    return render_template("home.html", habits=habits,title="Habit tracker - Home",selected_date=selected_date)
+    return render_template("home.html", habits=habits,title="Habit tracker - Home",selected_date=selected_date,completions=completions[selected_date])
 
 @app.route("/add",methods=["GET","POST"])
 def add_habit():
     if request.method == "POST":
         habits.append(request.form.get("habit"))
     return render_template("add_habit.html",title="Habit Tracker - Add Habit",selected_date=datetime.date.today())
+
+@app.route("/complete",methods=["Post"])
+def complete():
+    date_string = request.form.get("date")
+    habit = request.form.get("habitName")
+    date=datetime.date.fromisoformat(date_string)
+    completions[date].append(habit)
+    
+    return redirect(url_for("home",date=date_string))
