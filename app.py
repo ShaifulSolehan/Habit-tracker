a41a76c391f3408c5da1e602bcc80b53205b5ca2
ShaifulSolehan <shaifulsolehan@gmail.com>
2024-02-17 11:03:01 +0800

    Change to read from mongoDB instead of local variable

--- app.py
+++ app.py
@@ -1,40 +1,59 @@
+import os
 import datetime
+import uuid
+import certifi
 from collections import defaultdict
 from flask import Flask,render_template,request,redirect,url_for
+from pymongo import MongoClient
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__,template_folder="templates")
-
-habits=["testing","testing 2"]
-completions = defaultdict(list)
+client = MongoClient("mongodb+srv://Shaiful:saiful2206@entry.ay9oa2w.mongodb.net/",tlsCAFile = certifi.where())
+app.db=client.habit_tracker
 
 @app.context_processor
 def add_cal_date_range():
-    def date_range(start: datetime.date):
+    def date_range(start: datetime.datetime):
         dates = [start + datetime.timedelta(days=diff) for diff in range(-3,4)]
         return dates
     return {"date_range":date_range}
+
+def today_at_midnight():
+    today = datetime.datetime.today()
+    return datetime.datetime(today.year, today.month, today.day)
 
 @app.route("/")
 def home():
     date_str = request.args.get("date")
     if(date_str):
-        selected_date = datetime.date.fromisoformat(date_str)
+        selected_date = datetime.datetime.fromisoformat(date_str)
     else:
-        selected_date = datetime.date.today()
+        selected_date = datetime.datetime.today()
 
-    return render_template("home.html", habits=habits,title="Habit tracker - Home",selected_date=selected_date,completions=completions[selected_date])
+    habits_on_date = app.db.habits.find({"added":{"$lte":selected_date}})
+    completions = [
+        habit["habit"]
+        for habit in app.db.completions.find({"date":selected_date})
+    ]    
+    return render_template("home.html", habits=habits_on_date,title="Habit tracker - Home",selected_date=selected_date,completions=completions)
 
 @app.route("/add",methods=["GET","POST"])
 def add_habit():
+    today = today_at_midnight()
+
     if request.method == "POST":
-        habits.append(request.form.get("habit"))
-    return render_template("add_habit.html",title="Habit Tracker - Add Habit",selected_date=datetime.date.today())
+        app.db.habits.insert_one(
+            {"id":uuid.uuid4().hex, "added":today, "name":request.form.get("habit")}
+        )
+    return render_template("add_habit.html",title="Habit Tracker - Add Habit",selected_date=today)
 
 @app.route("/complete",methods=["Post"])
 def complete():
     date_string = request.form.get("date")
-    habit = request.form.get("habitName")
-    date=datetime.date.fromisoformat(date_string)
-    completions[date].append(habit)
+    habit = request.form.get("habitId")
+    date=datetime.datetime.fromisoformat(date_string)
+    app.db.completions.insert_one({"date":date, "habit":habit})
     
     return redirect(url_for("home",date=date_string))
